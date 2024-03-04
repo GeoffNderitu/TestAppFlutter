2341b9f673bc4f032dfe6a3b83ddbf2a548cd493
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-04 15:05:18 +0300

    New images and modified file

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+
+class MySokoAppImages{
+
+  static const String darkAppLogo = "assets/logos/my-soko-logo-dark.png";
+  static const String lightAppLogo = "assets/logos/my-soko-logo-light.png";
+
+
+  static const String google = "";
+  static const String facebook = "";
+}
