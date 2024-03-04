ae72f11622792369f9689da8de2f421b51f09e02
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-04 14:28:22 +0300

    Update in sizes file

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,83 @@
+
+class MySokosizes{
+
+  // App sizing
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+
+  // Icon sizing
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 44.0;
+  static const double iconLg = 32.0;
+
+
+  // Font sizing
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+
+
+  // Button sizing
+  static const double buttonHeight = 18.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonElevation = 4.0;
+
+
+  //AppBar size 
+  static const double appBarHeight = 56.0;
+
+
+  // thumbnail Sizing
+  static const double imageThumbSize = 80.0;
+
+
+  // Space sizing
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwnItems = 16.0;
+  static const double spaceBtwnSections = 32.0;
+
+
+  // BorderRadius sizing
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusmd = 8.0;
+  static const double borderRadiusLg= 12.0;
+
+
+  // Divider sizing
+  static const double dividerHeight = 1.0;
+
+  // Product image sizing
+  static const double productImageSize = 120.0;
+  static const double productImageRadius = 16.0;
+  static const double productItemHeight = 160.0;
+
+
+  // InputField sizing
+  static const double inputFieldRadius = 12.0;
+  static const double spaceBtwnInputFields = 16.0;
+
+
+  //Card sizes 
+  static const double cardRadiusLg = 16.0;
+  static const double cardRadiusMd = 12.0;
+  static const double cardRadiusSm = 10.0;
+  static const double cardElevation = 2.0;
+  static const double cardRadiusXs = 6.0;
+
+
+  // Image carousel height
+  static const double imageCarouselHeight = 200.0;
+
+  // Loaing indicator sizing
+  static const double loadingIndicatorSize = 36.0;
+
+
+  // GridView Spacing
+  static const double gridViewSpacing = 16.0;
+}
