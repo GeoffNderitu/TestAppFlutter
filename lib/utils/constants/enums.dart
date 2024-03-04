e8e0af41be8a6fb6f5b493c2a928fa9476b3f785
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-04 15:42:50 +0300

    Added content

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,9 @@
+
+
+enum TextSizes { small, medium, large}
+
+
+enum OrderStatus { processing, shipped, delivered}
+
+
+enum PaymentMethods { paypal, googlepay, applepay, visa, mastercard, creditcard, paystack, razorpay, paytm, mpesa}
