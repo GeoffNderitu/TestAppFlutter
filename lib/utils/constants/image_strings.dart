4e35275b49c2bc2eb68654de757325814df45f9d
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-06 17:17:09 +0300

    Changes

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,7 +2,7 @@
 class MySokoAppImages{
 
   static const String darkAppLogo = "assets/logos/my-soko-logo-dark.png";
-  static const String lightAppLogo = "assets/logos/my-soko-logo-light.png";
+  static const String lightAppLogo = "assets/logos/logo-color.png";
 
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logod/facebook-icon.png";
