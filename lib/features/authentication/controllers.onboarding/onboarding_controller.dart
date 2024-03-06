642bb470e56adc8c62db8a16660b4ea021bb3713
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-06 16:21:58 +0300

    New file

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  // Have Variables here
+
+  // Update the current index when page is scrolled
+  void updatePageIndicator(index) {}
+
+  // Jump to the specific dot of the selected page
+  void dotNavigationClick(index) {}
+
+  // Update the current index and jump to the next page
+  void nextPage() {}
+
+  // Update the Current Index and Jump to last page
+  void skipPage() {}
+}
