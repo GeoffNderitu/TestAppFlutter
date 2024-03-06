3fe071bf8cf70429a5307083d84b7f0290b1a1e0
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-06 16:49:34 +0300

    Changed

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,4 +1,8 @@
 import'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/constants/image_strings.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+import 'package:new_app_flutter/utils/constants/text_strings.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 
 
  class OnBoardingScreen extends StatelessWidget {
@@ -6,6 +10,39 @@ import'package:flutter/material.dart';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Horizontal scrollable Pages
+          PageView(
+            children: [
+              Column(
+                children: [
+                  Image(
+                    width: MySokoAppHelperFunctions.screenWidth() * 0.8,
+                    height: MySokoAppHelperFunctions.screenHeight() * 0.6,
+                    image: AssetImage(MySokoAppImages.onBoardingImage1),
+                  ),
+                  Text(
+                    MySokoAppTexts.onBoardingTitle1, 
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: MySokoSizes.spaceBtwnItems,
+                  ),
+
+                  Text(
+                    MySokoAppTexts.onBoardingSubTitle1, 
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
