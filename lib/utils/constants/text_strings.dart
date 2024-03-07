e690bad3d315065780d14a8ad22e0521d3b10293
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-07 21:43:52 +0300

    Typo fixed

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -8,7 +8,7 @@ class MySokoAppTexts{
   static const String onBoardingTitle3 = "Deliver to your doorstep";
 
   static const String onBoardingSubTitle1 = "Welcome to our world of MySoko - Get to grab what suits you Best!";
-  static const String onBoardingSubTitle2 = "We focus on offering stress-free transactions - Pick a conevnient Payment Method";
+  static const String onBoardingSubTitle2 = "We focus on offering stress-free transactions - Pick a convenient Payment Method";
   static const String onBoardingSubTitle3 = "Have Your Order Delivered to Your Doorstep in a Fast and Secure way!";
 
   // Home
