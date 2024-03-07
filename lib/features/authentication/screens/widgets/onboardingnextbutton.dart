fe35e50d56a1e23e688716ce81939e0373f3f63f
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-07 23:45:39 +0300

    New file with context

---
+++ lib/features/authentication/screens/widgets/onboardingnextbutton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:new_app_flutter/utils/constants/colors.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+import 'package:new_app_flutter/utils/device/device_utility.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MySokoAppHelperFunctions.isDarkMode(context);
+    return Positioned(
+      right: MySokoSizes.defaultSpace,
+      bottom: MySokoAppDeviceUtils.getBottomnavigationBarHeight(),
+      child: ElevatedButton(
+         onPressed: (){},   
+         style: ElevatedButton.styleFrom(shape: const CircleBorder(), backgroundColor: dark ? MySokoAppColors.primary : Colors.black),
+         child: const Icon(Iconsax.arrow_right_3),
+    ),
+          );
+  }
+}
