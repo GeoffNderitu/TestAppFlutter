9a5f1efa3bfa25ffa6f3bc7cc1c4fb6d349c4f4f
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-07 21:27:41 +0300

    New Onboarding images and text added

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -15,31 +15,61 @@ import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
         children: [
           // Horizontal scrollable Pages
           PageView(
-            children: [
-              Column(
-                children: [
-                  Image(
-                    width: MySokoAppHelperFunctions.screenWidth() * 0.8,
-                    height: MySokoAppHelperFunctions.screenHeight() * 0.6,
-                    image: AssetImage(MySokoAppImages.onBoardingImage1),
-                  ),
-                  Text(
-                    MySokoAppTexts.onBoardingTitle1, 
-                    style: Theme.of(context).textTheme.headlineMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: MySokoSizes.spaceBtwnItems,
-                  ),
-
-                  Text(
-                    MySokoAppTexts.onBoardingSubTitle1, 
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )
+            children: const[
+              OnBoardingPage(
+                image: MySokoAppImages.onBoardingImage1, 
+                title: MySokoAppTexts.onBoardingTitle1,
+                subTitle: MySokoAppTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: MySokoAppImages.onBoardingImage2, 
+                title: MySokoAppTexts.onBoardingTitle2,
+                subTitle: MySokoAppTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: MySokoAppImages.onBoardingImage3, 
+                title: MySokoAppTexts.onBoardingTitle3,
+                subTitle: MySokoAppTexts.onBoardingSubTitle3,
+              ),
             ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key, required this.image, required this.title, required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(MySokoSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+            width: MySokoAppHelperFunctions.screenWidth() * 0.8,
+            height: MySokoAppHelperFunctions.screenHeight() * 0.6,
+            image: AssetImage(image),
+          ),
+          Text(
+           title, 
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: MySokoSizes.spaceBtwnItems,
+          ),
+      
+          Text(
+            subTitle, 
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
           ),
         ],
       ),
