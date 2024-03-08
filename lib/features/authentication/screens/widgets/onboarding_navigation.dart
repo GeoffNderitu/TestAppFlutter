3c74804bf114d90da2c5e2b6168843d8305bf0bd
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-08 22:57:32 +0300

    Changes

--- lib/features/authentication/screens/widgets/onboarding_navigation.dart
+++ lib/features/authentication/screens/widgets/onboarding_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_app_flutter/features/authentication/controllers.onboarding/onboarding_controller.dart';
 import 'package:new_app_flutter/utils/constants/colors.dart';
 import 'package:new_app_flutter/utils/constants/sizes.dart';
 import 'package:new_app_flutter/utils/device/device_utility.dart';
@@ -12,12 +13,14 @@ class OnBoardingNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = OnBoardingController.instance;
     final dark = MySokoAppHelperFunctions.isDarkMode(context);
     return Positioned(
       bottom: MySokoAppDeviceUtils.getBottomnavigationBarHeight() + 25,
       left: MySokoSizes.defaultSpace,
       child: SmoothPageIndicator(
-        controller: PageController(), 
+        controller: controller.pageController, 
+        onDotClicked: controller.dotNavigationClick,
         count: 3, 
         effect: ExpandingDotsEffect(activeDotColor: dark ? MySokoAppColors.light: MySokoAppColors.dark, dotHeight: 6),
         ),
