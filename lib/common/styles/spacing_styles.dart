372a1663d96dee3117d215745185669ce2eea32b
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-08 23:46:35 +0300

    Created A new File

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+
+class MySokoAppSpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: MySokoSizes.appBarHeight,
+    left: MySokoSizes.defaultSpace,
+    bottom: MySokoSizes.defaultSpace,
+    right: MySokoSizes.defaultSpace,
+  );
+}
