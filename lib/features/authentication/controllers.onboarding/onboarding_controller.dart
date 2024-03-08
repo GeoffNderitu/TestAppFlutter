7adbe5b7db2219713daeceea4bff9b57132ba85c
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-08 22:56:29 +0300

    Changes

--- lib/features/authentication/controllers.onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -12,14 +12,27 @@ class OnBoardingController extends GetxController {
 
 
   // Update the current index when page is scrolled
-  void updatePageIndicator(index) {}
+  void updatePageIndicator(index) => currentPageIndex = index;
 
   // Jump to the specific dot of the selected page
-  void dotNavigationClick(index) {}
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
 
   // Update the current index and jump to the next page
-  void nextPage() {}
+  void nextPage() {
+    if(currentPageIndex.value == 2){
+      // Get.to(LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
 
   // Update the Current Index and Jump to last page
-  void skipPage() {}
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
 }
