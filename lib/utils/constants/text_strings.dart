9a556fbc334294f7796e070e9efd412a12ea35e9
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-11 10:30:13 +0300

    New variables added

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -1,6 +1,12 @@
 
 class MySokoAppTexts{
 
+  static const String and = "and";
+  static const String skip = "Skip";
+  static const String done = "Done";
+  static const String submit = "Submit";
+  static const String appName = "MySoko";
+  static const String mySokoAppContinue = "Continue";
 
   // On-boarding texts
   static const String onBoardingTitle1 = "Pick your product";
@@ -11,8 +17,45 @@ class MySokoAppTexts{
   static const String onBoardingSubTitle2 = "We focus on offering stress-free transactions - Pick a convenient Payment Method";
   static const String onBoardingSubTitle3 = "Have Your Order Delivered to Your Doorstep in a Fast and Secure way!";
 
+  // -- Authentication Forms
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of use";
+  static const String verificationCode = "verificationCode";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+
+  // -- Authentication Headings
   static const String loginTitle = "It's Nice to see you again";
   static const String loginSubTitle = "Experience Our Top-Tier Services Today!";
+  static const String signupTitle = "It's time to Create your account";
+  static const String forgetPasswordTitle = "Forget password";
+  static const String forgetPasswordSubTitle = "Don't panic, we alll forget sometimes. Fill in your email and we'll you send a password reset link";
+  static const String changeYourPasswordTitle = "Password Reset Link Sent!";
+  static const String changeYourPasswordSubTitle = "We Take Your ACcount's Security As Our Priority! We've Sent a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle = "You Did It! Your Account Is Almost Ready Just Verify Your Email To Start Shopping With Us And Get Outstanding Deals and Offers";
+  static const String emailNotReceivedMessage = "Oops! Missed the email? Check your junk/spam or resend it.";
+  static const String yourAccountCreatedTitle = "Congratulations! Your account was successfully created!";
+  static const String yourAccountCreatedSubTitle = "Welcome to MySoko Your Perfect Destination: Get To Experience What A Beautiful And Customer-Friendly Experience Our Platform Has To Offer!";
+
+
+  // -- Product
+  static const String popularProducts = "Popular Products";
+
   // Home
   // static const String onBoardinghomeAppBarTitle = "Come on! Take a Look Around :)";
   // static const String onBoardingAppTitle1 = "Geoffrey N Ngugi";
