36744aeb6dbaffa939cf93e436c1946c1f04c284
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-12 23:53:26 +0300

    Other Changes

--- lib/features/authentication/screens/routes/routes.dart
+++ lib/features/authentication/screens/routes/routes.dart
@@ -1,5 +1,7 @@
 class RoutesClass {
   static String home = "/";
   static String logInScreen = "/logInScreen";
- 
+  
+  static String getHomeRoute()=>home;
+  static String getLogInScreen()=>logInScreen;
 }
