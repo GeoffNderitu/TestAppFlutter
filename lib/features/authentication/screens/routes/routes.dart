1bdaf643fae9b74a564121b6e271ec0d2c825392
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-12 23:56:00 +0300

    List For The Routes

--- lib/features/authentication/screens/routes/routes.dart
+++ lib/features/authentication/screens/routes/routes.dart
@@ -1,7 +1,14 @@
+import 'package:get/get.dart';
+
 class RoutesClass {
-  static String home = "/";
+  // static String home = "/";
   static String logInScreen = "/logInScreen";
   
-  static String getHomeRoute()=>home;
+  // static String getHomeRoute()=>home;
   static String getLogInScreen()=>logInScreen;
+
+  static List<GetPage> routes =[
+    // GetPage(name: home, page: ()=>const HomeScreen),
+  
+  ];
 }
