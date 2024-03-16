90238f3c10280e68fe7543ead698e1f9e319227b
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-16 11:28:58 +0300

    Added image icons at footer

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -101,8 +101,30 @@ class LoginScreen extends StatelessWidget {
               ),
 
               // Footer Section
-
-              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: MySokoAppColors.grey), borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(onPressed: () {}, icon: const Image(
+                      width: MySokoSizes.iconMd,
+                      height: MySokoSizes.iconMd,
+                      image: AssetImage(MySokoAppImages.google),
+                      ),
+                      ),
+                  ),
+                  const SizedBox(width: MySokoSizes.spaceBtwnItems),
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: MySokoAppColors.grey), borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(onPressed: () {}, icon: const Image(
+                      width: MySokoSizes.iconMd,
+                      height: MySokoSizes.iconMd,
+                      image: AssetImage(MySokoAppImages.facebook),
+                      ),
+                      ),
+                  ),
+                ],
+              )
             ],
           ),          
           ),
