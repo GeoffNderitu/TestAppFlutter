40d455b535cb8c279ca329410800411d02088b03
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-16 16:55:49 +0300

    Modified file

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:new_app_flutter/common/widgets/login_signup/form_divider.dart';
+import 'package:new_app_flutter/common/widgets/login_signup/social_btns.dart';
+import 'package:new_app_flutter/features/authentication/screens/signup/widgets/signup_form.dart';
 import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 
 import '../../../../utils/constants/colors.dart';
@@ -19,105 +23,20 @@ class SignUpScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(padding: const EdgeInsets.all(MySokoSizes.defaultSpace),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(MySokoAppTexts.signupTitle, style: Theme.of(context).textTheme.headlineMedium),
             const SizedBox(height: MySokoSizes.spaceBtwnSections),
 
             // Form
-            Form(child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.firstName,
-                          prefixIcon: Icon(Iconsax.user),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: MySokoSizes.spaceBtwnInputFields),
-
-                    // Last name
-                    Expanded(
-                      child: TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.lastName,
-                          prefixIcon: Icon(Iconsax.user),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                    const SizedBox(height: MySokoSizes.spaceBtwnInputFields),
-                  // username
-                    TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.username,
-                          prefixIcon: Icon(Iconsax.user_edit),
-                        ),
-                      ),
-                      const SizedBox(height: MySokoSizes.spaceBtwnInputFields),
-                      // Email 
-                      TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.email,
-                          prefixIcon: Icon(Iconsax.direct),
-                        ),
-                      ),
-                       const SizedBox(height: MySokoSizes.spaceBtwnInputFields),
-
-                      // Phone No.
-                      TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.phoneNo,
-                          prefixIcon: Icon(Iconsax.call),
-                        ),
-                      ),
-                      const SizedBox(height: MySokoSizes.spaceBtwnInputFields),
-
-                      // Password
-                      TextFormField(
-                        expands: false,
-                        decoration: const InputDecoration(
-                          labelText: MySokoAppTexts.password,
-                          prefixIcon: Icon(Iconsax.password_check),
-                          suffixIcon: Icon(Iconsax.eye_slash),
-                        ),
-                      ),
-                      const SizedBox(height: MySokoSizes.spaceBtwnSections),
-
-                      // Ts & Cs Checkbox
-                      Row(children: [
-                         SizedBox(width: 24, height: 24, child: Checkbox(value: true, onChanged: (value){})),
-                        const SizedBox(width: MySokoSizes.spaceBtwnItems),
-                        Text.rich(TextSpan(
-                          children: [
-                            TextSpan(text: '${MySokoAppTexts.iAgreeTo} ', style: Theme.of(context).textTheme.bodySmall),
-                            TextSpan(text: MySokoAppTexts.privacyPolicy, style: Theme.of(context).textTheme.bodyMedium!.apply(
-                              color: dark ? MySokoAppColors.white : MySokoAppColors.primary,
-                              decoration: TextDecoration.underline,
-                              decorationColor: dark ? MySokoAppColors.white : MySokoAppColors.primary,
-                            )),
-                            TextSpan(text: '${MySokoAppTexts.and} ', style: Theme.of(context).textTheme.bodySmall),
-                            TextSpan(text: MySokoAppTexts.termsOfUse, style: Theme.of(context).textTheme.bodyMedium!.apply(
-                              color: dark ? MySokoAppColors.white : MySokoAppColors.primary,
-                              decoration: TextDecoration.underline,
-                              decorationColor: dark ? MySokoAppColors.white : MySokoAppColors.primary,
-                            )),
-                          ],
-                        ),
-                        ),
-                      ],
-                      ),
-                  ],
-                )
-              ),
+            const MySokoAppSignUpForm(),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
+              // Divider
+            MySokoAppFormDivider(dividerText: MySokoAppTexts.orSignUpWith.capitalize!),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
+              // Footer
+             const MySokoAppSocialButtons(),
+            
           ],
         ),
         ),
