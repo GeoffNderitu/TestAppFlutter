f60760bc4536dcdeb88d5174f6aa7abc4da4dafc
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-16 09:45:39 +0300

    Light and Dark

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:new_app_flutter/common/styles/spacing_styles.dart';
 import 'package:new_app_flutter/utils/constants/image_strings.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final dark = MySokoAppHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: MySokoAppSpacingStyle.paddingWithAppBarHeight,
@@ -18,7 +21,7 @@ class LoginScreen extends StatelessWidget {
                 children: [
                   Image(
                     height: 150,
-                    image: AssetImage(MySokoAppImages.darkAppLogo),              
+                    image: AssetImage(dark ? MySokoAppImages.lightAppLogo : MySokoAppImages.darkAppLogo),              
                   )
                 ],
               )
