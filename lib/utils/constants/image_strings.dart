cd3c4e051442e9adec0d7ee3bbbc8a46a6b056db
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-16 11:33:45 +0300

    path for facebook icon corrected

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -5,7 +5,7 @@ class MySokoAppImages{
   static const String lightAppLogo = "assets/logos/logo-color.png";
 
   static const String google = "assets/logos/google-icon.png";
-  static const String facebook = "assets/logos/facebook-icon.png";
+  static const String facebook = "assets/logos/facebook_icon.png";
 
   static const String onBoardingImage1 = "assets/images/on_boarding_images/juicy-girl-and-boy-searching-for-the-right-files.gif";
   static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-woman-in-shopping-cart.gif";
