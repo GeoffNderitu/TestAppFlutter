457bb630836129deeea84e17bb0806860f47af7a
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-16 14:44:09 +0300

    new widgets added

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:new_app_flutter/common/styles/spacing_styles.dart';
+import 'package:new_app_flutter/common/widgets/login_signup/form_divider.dart';
+import 'package:new_app_flutter/common/widgets/login_signup/social_btns.dart';
 import 'package:new_app_flutter/features/authentication/screens/login/widgets/login_form.dart';
 import 'package:new_app_flutter/features/authentication/screens/login/widgets/login_form_header.dart';
 import 'package:new_app_flutter/utils/constants/colors.dart';
@@ -16,8 +18,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final dark = MySokoAppHelperFunctions.isDarkMode(context);
-
+    
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -30,43 +31,14 @@ class LoginScreen extends StatelessWidget {
               const MySokoAppLoginForm(),
 
               // Divider
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(child: Divider(color: dark ? MySokoAppColors.darkGrey: MySokoAppColors.grey, thickness: 0.5, indent: 60, endIndent: 5)),
-                  Text(MySokoAppTexts.orSignInWith.capitalize!, style: Theme.of(context).textTheme.labelMedium),
-                  Flexible(child: Divider(color: dark ? MySokoAppColors.darkGrey: MySokoAppColors.grey, thickness: 0.5, indent: 5, endIndent: 60)),
-                ],
-              ),
+              MySokoAppFormDivider(dividerText: MySokoAppTexts.orSignInWith.capitalize!),
+
               const SizedBox(height: MySokoSizes.spaceBtwnSections),
               // Footer Section
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(border: Border.all(color: MySokoAppColors.grey), borderRadius: BorderRadius.circular(100)),
-                    child: IconButton(onPressed: () {}, icon: const Image(
-                      width: MySokoSizes.iconMd,
-                      height: MySokoSizes.iconMd,
-                      image: AssetImage(MySokoAppImages.google),
-                      ),
-                      ),
-                  ),
-                  const SizedBox(width: MySokoSizes.spaceBtwnItems),
-                  Container(
-                    decoration: BoxDecoration(border: Border.all(color: MySokoAppColors.grey), borderRadius: BorderRadius.circular(100)),
-                    child: IconButton(onPressed: () {}, icon: const Image(
-                      width: MySokoSizes.iconMd,
-                      height: MySokoSizes.iconMd,
-                      image: AssetImage(MySokoAppImages.facebook),
-                      ),
-                      ),
-                  ),
-                ],
-              )
-            ],
-          ),          
-          ),
+              const MySokoAppSocialButtons(),
+              ],
+            ),          
+         ),
       ),
     );
   }
