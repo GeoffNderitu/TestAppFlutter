32227e99851b22f156eee482d56b4a0225e03547
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-17 22:59:04 +0300

    Title and Subtitle section complete

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:new_app_flutter/features/authentication/screens/login/login.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 
+import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 
 class VerifyEmailScreen extends StatelessWidget {
@@ -12,18 +16,27 @@ class VerifyEmailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         actions: [
-          IconButton(onPressed: () => Get.back(), icon: const Icon(CupertinoIcons.clear))
+          IconButton(onPressed: () => Get.offAll(() => const LoginScreen()), icon: const Icon(CupertinoIcons.clear))
         ],
       ),
-      body: const SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.all(MySokoSizes.defaultSpace),
+      body:  SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(MySokoSizes.defaultSpace),
         child: Column(
           children: [
             // Image
-
+            Image(image: const AssetImage(MySokoAppImages.deliveredEmailIllustration),
+                  width: MySokoAppHelperFunctions.screenWidth() * 0.6,
+            ),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
             // Title and Subtitle
-
+            Text(MySokoAppTexts.confirmEmail, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
+            const SizedBox(height: MySokoSizes.spaceBtwnItems),
+            Text('support@MySokoApp.com', style: Theme.of(context).textTheme.labelLarge, textAlign: TextAlign.center),
+            const SizedBox(height: MySokoSizes.spaceBtwnItems),
+            Text(MySokoAppTexts.confirmEmailSubTitle, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
             // Btns
 
           ],
