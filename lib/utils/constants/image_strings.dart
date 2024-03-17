6e9db6ccf83d4320104152e30722ba8501fbcbf3
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-17 21:36:06 +0300

    Image paths changed

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -11,10 +11,10 @@ class MySokoAppImages{
   static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-woman-in-shopping-cart.gif";
   static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-man-delivery.gif";
 
-  static const String productIllustration = "assets/images/animations/sammy-guy-doing-workout-with-a-tire.png";
-  static const String productSaleIllustration = "assets/images/animations/sammy-34.png";
-  static const String staticSuccessIllustration = "assets/images/animations/bubble-gum-man-successfully-crossing-finish-line.gif";
-  static const String deliveredIllustration = "assets/images/animations/sammy-come-back-later.png";
-  static const String deliveredEmailIllustration = "assets/images/animations/bubble-gum-message-sent.gif";
-  static const String verifyIllustration = "assets/images/animations/sammy-man-is-going-on-a-trip-with-a-suitcase-and-a-passport.png";
+  static const String productIllustration = "images/animations/sammy-guy-doing-workout-with-a-tire.png";
+  static const String productSaleIllustration = "images/animations/sammy-34.png";
+  static const String staticSuccessIllustration = "images/animations/bubble-gum-man-successfully-crossing-finish-line.gif";
+  static const String deliveredIllustration = "images/animations/sammy-come-back-later.png";
+  static const String deliveredEmailIllustration = "images/animations/bubble-gum-message-sent.gif";
+  static const String verifyIllustration = "images/animations/sammy-man-is-going-on-a-trip-with-a-suitcase-and-a-passport.png";
 }
