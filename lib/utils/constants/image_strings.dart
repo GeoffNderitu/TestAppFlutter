5249eb3ad34d4a5bc1c22a0580edcae043198296
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-17 20:43:16 +0300

    Updated variables

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -10,4 +10,11 @@ class MySokoAppImages{
   static const String onBoardingImage1 = "assets/images/on_boarding_images/juicy-girl-and-boy-searching-for-the-right-files.gif";
   static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-woman-in-shopping-cart.gif";
   static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-man-delivery.gif";
+
+  static const String productIllustration = "assets/images/animations/sammy-guy-doing-workout-with-a-tire.png";
+  static const String productSaleIllustration = "assets/images/animations/sammy-34.png";
+  static const String staticSuccessIllustration = "assets/images/animations/bubble-gum-man-successfully-crossing-finish-line.gif";
+  static const String deliveredIllustration = "assets/images/animations/sammy-come-back-later.png";
+  static const String deliveredEmailIllustration = "assets/images/animations/bubble-gum-message-sent.gif";
+  static const String verifyIllustration = "assets/images/animations/sammy-man-is-going-on-a-trip-with-a-suitcase-and-a-passport.png";
 }
