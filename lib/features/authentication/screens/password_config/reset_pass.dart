f2d14c21b953e116ef9c66a1a4de0c8834aecbfe
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-18 19:25:49 +0300

    New mods

---
+++ lib/features/authentication/screens/password_config/reset_pass.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SingleChildScrollView(
+        child: Padding(padding: EdgeInsets.all(MySokoSizes.defaultSpace),
+        child: Column(
+          children: [
+            // Image 
+            // Title and Subtitle
+            // Btns
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
