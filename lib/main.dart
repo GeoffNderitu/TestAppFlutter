695c73b62ec829b2129d5c1aacec51e211e6a1e2
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-18 19:07:43 +0300

    New route added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:new_app_flutter/common/widgets/success_screen/success_screen.dart';
 import 'package:new_app_flutter/features/authentication/screens/login/login.dart';
+import 'package:new_app_flutter/features/authentication/screens/password_config/forget_pass.dart';
 import 'package:new_app_flutter/features/authentication/screens/signup/signup.dart';
 import 'package:new_app_flutter/features/authentication/screens/signup/verify_email.dart';
 import 'package:new_app_flutter/utils/theme/theme.dart';
@@ -16,6 +17,7 @@ void main() {
       '/SignUpScreen':(context) => SignUpScreen(),
       '/VerifyEmailScreen':(context) => VerifyEmailScreen(),
       // '/SuccessScreen':(context) => SuccessScreen(),
+      '/ForgetPassword':(context) => ForgetPassword(),
     },
   ));
 } 
