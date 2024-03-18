776412019f2eaa3a47a5902d5c34f075019e3800
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-18 19:47:41 +0300

    Added Btn and resend email

--- lib/features/authentication/screens/password_config/reset_pass.dart
+++ lib/features/authentication/screens/password_config/reset_pass.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:new_app_flutter/utils/constants/sizes.dart';
+
+import '../../../../utils/constants/image_strings.dart';
+import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 class ResetPasswordScreen extends StatelessWidget {
   const ResetPasswordScreen({super.key});
@@ -7,14 +13,32 @@ class ResetPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.all(MySokoSizes.defaultSpace),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: () => Get.back(), icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(MySokoSizes.defaultSpace),
         child: Column(
           children: [
-            // Image 
+            Image(image: const AssetImage(MySokoAppImages.deliveredEmailIllustration),
+                  width: MySokoAppHelperFunctions.screenWidth() * 0.6,
+            ),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
             // Title and Subtitle
-            // Btns
+            Text(MySokoAppTexts.changeYourPasswordTitle, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
+            const SizedBox(height: MySokoSizes.spaceBtwnItems),
+            Text(MySokoAppTexts.changeYourPasswordSubTitle, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center),
+            const SizedBox(height: MySokoSizes.spaceBtwnSections),
+
+            // Btn
+            SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: const Text(MySokoAppTexts.done)),
+            ),
+            const SizedBox(height: MySokoSizes.spaceBtwnItems),
+            SizedBox(width: double.infinity, child: TextButton(onPressed: () {}, child: const Text(MySokoAppTexts.resendEmail)),
+            ),
           ],
         ),
         ),
