8599ba1b15d1f93b93fe67d9383badf0e1ae588c
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-18 19:02:50 +0300

    Headings added

---
+++ lib/features/authentication/screens/password_config/forget_pass.dart
@@ -0,0 +1,30 @@
+import'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+
+import '../../../../utils/constants/text_strings.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:  Padding(padding: 
+      const EdgeInsets.all(MySokoSizes.defaultSpace),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Headings
+          Text(MySokoAppTexts.forgetPasswordTitle, style: Theme.of(context).textTheme.headlineMedium),
+          const SizedBox(height: MySokoSizes.spaceBtwnItems),
+          Text(MySokoAppTexts.forgetPasswordSubTitle, style: Theme.of(context).textTheme.labelMedium),
+          const SizedBox(height: MySokoSizes.spaceBtwnSections * 2),
+          // Text Field area
+          // Submit Btn
+        ],
+      ),
+      ),
+    );
+  }
+}
