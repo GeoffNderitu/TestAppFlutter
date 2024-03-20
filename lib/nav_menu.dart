3047feb697078de66f66e2b5d5334a1018aff538
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-20 20:40:50 +0300

    Different modes added (light and dark)

--- lib/nav_menu.dart
+++ lib/nav_menu.dart
@@ -1,6 +1,9 @@
  import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:new_app_flutter/features/shop/screens/home/home.dart';
+import 'package:new_app_flutter/utils/constants/colors.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 
  class NavigationMenu extends StatelessWidget {
    const NavigationMenu({super.key});
@@ -8,7 +11,7 @@ import 'package:iconsax/iconsax.dart';
    @override
    Widget build(BuildContext context) {
     final controller = Get.put(NaviGationController());
-
+    final darkMode = MySokoAppHelperFunctions.isDarkMode(context);
      return Scaffold(
       bottomNavigationBar: Obx(
         () => NavigationBar(
@@ -16,7 +19,8 @@ import 'package:iconsax/iconsax.dart';
           elevation: 0,
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected: (index) => controller.selectedIndex.value = index,
-        
+          backgroundColor: darkMode ? MySokoAppColors.black : Colors.white,
+          indicatorColor: darkMode ? MySokoAppColors.white.withOpacity(0.1) : MySokoAppColors.black.withOpacity(0.1),
           destinations: const [
            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Shop'),
@@ -25,7 +29,7 @@ import 'package:iconsax/iconsax.dart';
         ],
         ),
       ),
-      body: controller.screens[controller.selectedIndex.value],
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
      );
    }
  }
@@ -34,5 +38,5 @@ import 'package:iconsax/iconsax.dart';
  class NaviGationController extends GetxController{
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [Container(color: Color.fromARGB(255, 177, 46, 46)), Container(color: Color.fromARGB(255, 94, 209, 79)), Container(color: Color.fromARGB(255, 26, 197, 189)), Container(color: Color.fromARGB(255, 136, 117, 104))];
+  final screens = [const HomeScreen(), Container(color: Color.fromARGB(255, 94, 209, 79)), Container(color: Color.fromARGB(255, 26, 197, 189)), Container(color: Color.fromARGB(255, 136, 117, 104))];
  }
