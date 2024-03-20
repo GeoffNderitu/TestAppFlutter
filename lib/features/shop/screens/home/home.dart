bfd00bd51662775e84f44d6d5209a6826492005f
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-20 22:26:34 +0300

    updated file

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import '../../../../utils/constants/colors.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body:Column(
+        children: [
+          Container(
+            color: MySokoAppColors.primary,
+            padding: const EdgeInsets.all(0),
+            child: Stack(
+              children: [
+                MySokoAppCircularContainer(backgroundColor: MySokoAppColors.textWhite.withOpacity(0.1)),
+                
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
