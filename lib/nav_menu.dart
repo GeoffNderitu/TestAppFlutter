5450e3743d16886720f1f793503fc31079adc775
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-20 20:23:09 +0300

    Added observer

--- lib/nav_menu.dart
+++ lib/nav_menu.dart
@@ -1,4 +1,5 @@
  import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
  class NavigationMenu extends StatelessWidget {
@@ -6,14 +7,32 @@ import 'package:iconsax/iconsax.dart';
  
    @override
    Widget build(BuildContext context) {
+    final controller = Get.put(NaviGationController());
+
      return Scaffold(
-      bottomNavigationBar: NavigationBar(destinations: const [
-         NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-         NavigationDestination(icon: Icon(Iconsax.shop), label: 'Shop'),
-         NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-         NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-      ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+        
+          destinations: const [
+           NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+           NavigationDestination(icon: Icon(Iconsax.shop), label: 'Shop'),
+           NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+           NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+        ],
+        ),
       ),
+      body: Container(),
      );
    }
+ }
+
+
+ class NaviGationController extends GetxController{
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [Container(color: Color.fromARGB(255, 177, 46, 46)), Container(color: Color.fromARGB(255, 94, 209, 79)), Container(color: Color.fromARGB(255, 26, 197, 189)), Container(color: Color.fromARGB(255, 136, 117, 104))];
  }
