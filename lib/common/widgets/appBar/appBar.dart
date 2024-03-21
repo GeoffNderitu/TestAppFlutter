94064e84d114c0caa9e2902f2382c0dd5f54b617
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-21 21:14:47 +0300

    Changes added

--- lib/common/widgets/appBar/appBar.dart
+++ lib/common/widgets/appBar/appBar.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:new_app_flutter/utils/device/device_utility.dart';
 
+import '../../../utils/constants/sizes.dart';
+
 class MsAppBar extends StatelessWidget implements PreferredSizeWidget{
-  const MsAppBar({super.key});
+  const MsAppBar({
+    super.key,
+    this.title,
+    this.actions,
+    this.leadingIcon,
+    this.leadingOnPressed,
+    this.showBackArrow = false,  
+  });
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPressed;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(padding: const EdgeInsets.symmetric(horizontal: MySokoSizes.md),
+    child: AppBar(
+      automaticallyImplyLeading: false,
+      leading: showBackArrow 
+          ? IconButton(onPressed: () => Get.back(), icon: const Icon(Iconsax.arrow_left))
+          : leadingIcon != null ? IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon)) : null,
+          title: title,
+          actions: actions,
+
+    ),
+    );
   }
   
   @override
