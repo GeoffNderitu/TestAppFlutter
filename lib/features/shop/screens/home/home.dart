964cd6862fc4dfe67bac7ae68d1ea196271995cb
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-21 20:32:31 +0300

    const keyword added

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -13,13 +13,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body:SingleChildScrollView(
         child: Column(
           children: [
             MsPrimaryHeaderContainer(child: Column(
               children: [
-
+                
               ],
             )),
           ],
