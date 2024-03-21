ee75ae3590354cb4a70032ba17fc3bc705f58123
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-21 21:27:18 +0300

    Typo corrected

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:new_app_flutter/common/widgets/appBar/appBar.dart';
+import 'package:new_app_flutter/common/widgets/appBar/appbar.dart';
 import 'package:new_app_flutter/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
