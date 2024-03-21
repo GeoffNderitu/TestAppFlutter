0d9fb219a85295438baa4088268525d27a5c346d
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-21 20:48:13 +0300

    File renamed

--- lib/common/widgets/appBar/appBar.dart
+++ lib/common/widgets/appBar/appBar.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/device/device_utility.dart';
+
+class MsAppBar extends StatelessWidget implements PreferredSizeWidget{
+  const MsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+  
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(MySokoAppDeviceUtils.getAppBarHeight());
+
+}
