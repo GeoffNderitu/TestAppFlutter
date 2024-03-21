eb0badea7e16372c3f8dd539034911da46974e6d
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-21 19:59:12 +0300

    Added a clipper

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:new_app_flutter/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../utils/constants/colors.dart';
@@ -9,19 +12,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        children: [
-          Container(
-            color: MySokoAppColors.primary,
-            padding: const EdgeInsets.all(0),
-            child: Stack(
-              children: [
-                MySokoAppCircularContainer(backgroundColor: MySokoAppColors.textWhite.withOpacity(0.1)),
-                
-              ],
-            ),
-          )
-        ],
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: MySokoAppCustomCurvedEdges(),
+              child: Container(
+                color: MySokoAppColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 300,
+                  child: Stack(
+                    children: [
+                      Positioned(top:-150, right: -250, child: MySokoAppCircularContainer(backgroundColor: MySokoAppColors.textWhite.withOpacity(0.1))),
+                      Positioned(top:100, right: -300, child: MySokoAppCircularContainer(backgroundColor: MySokoAppColors.textWhite.withOpacity(0.1))),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
