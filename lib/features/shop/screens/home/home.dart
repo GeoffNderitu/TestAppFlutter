b7fd7179d6999cd85c6ab655a9459ba125699ea3
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-22 21:31:31 +0300

    Added headline texts

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:new_app_flutter/common/widgets/appBar/appbar.dart';
+import 'package:new_app_flutter/common/widgets/app_bar/app_bar.dart';
 import 'package:new_app_flutter/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
@@ -20,18 +19,16 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             MsPrimaryHeaderContainer(
-              child: Column(
-                children: [
-                  MsAppBar(
-                    title: Column(
-                      children: [
-                        Text(MySokoAppTexts.homeAppBarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: MySokoAppColors.grey)),
-                        Text(MySokoAppTexts.homeAppBarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: MySokoAppColors.white)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+             child: Column(
+              children: [
+                MsAppBar(title: Column(
+                  children: [
+                    Text(MySokoAppTexts.homeAppBarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: MySokoAppColors.grey)),
+                    Text(MySokoAppTexts.homeAppBarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: MySokoAppColors.white),),
+                  ],
+                ),)
+              ],
+             ),
             )
           ],
         ),
