aaa9749f5626f10fcc8663ab7a0fd8cb299cb9bf
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-22 21:42:55 +0300

    Added a little basket with a counter

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:new_app_flutter/common/widgets/app_bar/app_bar.dart';
 import 'package:new_app_flutter/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
@@ -22,11 +23,34 @@ class HomeScreen extends StatelessWidget {
              child: Column(
               children: [
                 MsAppBar(title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(MySokoAppTexts.homeAppBarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: MySokoAppColors.grey)),
                     Text(MySokoAppTexts.homeAppBarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: MySokoAppColors.white),),
                   ],
-                ),)
+                ),
+                actions: [
+                  Stack(
+                    children: [
+                      IconButton(onPressed: () {}, icon: const Icon(Iconsax.shopping_bag, color: MySokoAppColors.white)),
+                      Positioned(
+                        right: 0,
+                        child: Container(
+                          width: 18,
+                          height: 18,
+                          decoration: BoxDecoration(
+                            color: MySokoAppColors.black,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Center(
+                            child: Text('2', style: Theme.of(context).textTheme.labelLarge!.apply(color: MySokoAppColors.white)),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+                )
               ],
              ),
             )
