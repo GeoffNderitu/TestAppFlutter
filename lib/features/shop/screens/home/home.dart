32726b8d1fb1def8b950c2683299235cabc04d40
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-22 22:47:50 +0300

    Extraction of widget

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -4,11 +4,14 @@ import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:new_app_flutter/common/widgets/app_bar/app_bar.dart';
 import 'package:new_app_flutter/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:new_app_flutter/utils/device/device_utility.dart';
+import 'package:new_app_flutter/utils/helpers/helper_functions.dart';
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/constants/text_strings.dart';
 import 'widgets/home_app_bar.dart';
 
@@ -24,10 +27,52 @@ class HomeScreen extends StatelessWidget {
             MsPrimaryHeaderContainer(
              child: Column(
               children: [
-                MsHomeAppBar()
+                MsHomeAppBar(),
+                SizedBox(height: MySokoSizes.spaceBtwnSections),
+
+                // Searchbar goes here
+                MsSearchContainer(
+                  text: 'Search Store',
+                )
               ],
              ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MsSearchContainer extends StatelessWidget {
+  const MsSearchContainer({
+    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true,
+  });
+
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MySokoAppHelperFunctions.isDarkMode(context);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: MySokoSizes.defaultSpace),
+      child: Container(
+        width: MySokoAppDeviceUtils.getScreenwidth(context),
+        padding: const EdgeInsets.all(MySokoSizes.md),
+        decoration: BoxDecoration(
+          color: showBackground ? dark ? MySokoAppColors.dark : MySokoAppColors.light : Colors.transparent,
+          borderRadius: BorderRadius.circular(MySokoSizes.cardRadiusLg),
+          border: showBorder ? Border.all(color: MySokoAppColors.grey): null,
+        ),
+        child: Row(
+          children: [
+            const Icon(Iconsax.search_normal, color: MySokoAppColors.darkerGrey),
+            const SizedBox(width: MySokoSizes.spaceBtwnItems),
+            Text(text, style: Theme.of(context).textTheme.bodySmall),
           ],
         ),
       ),
