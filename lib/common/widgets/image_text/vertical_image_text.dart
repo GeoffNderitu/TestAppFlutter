2addc575dbd1a8815315a553e6a057c55d14c919
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-23 11:42:39 +0300

    Added COntent

---
+++ lib/common/widgets/image_text/vertical_image_text.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:new_app_flutter/utils/constants/sizes.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+
+class MsVerticalImageText extends StatelessWidget {
+  const MsVerticalImageText({
+    super.key,
+    required this.image, 
+    required this.title, 
+    this.textColor = MySokoAppColors.white, 
+    this.backgroundColor = MySokoAppColors.white, 
+    this.onTap,
+  });
+  
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MySokoAppHelperFunctions.isDarkMode(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: MySokoSizes.spaceBtwnItems),
+        child: Column(
+          children: [
+            // Circular icon section
+            Container(
+              width: 56,
+              height: 56,
+              padding: const EdgeInsets.all(MySokoSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor ?? (dark ? MySokoAppColors.black : MySokoAppColors.white),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: 
+                Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                  color: dark ? MySokoAppColors.light : MySokoAppColors.dark,
+                  )
+                  ),
+            ),
+            // Text Section
+            const SizedBox(height: MySokoSizes.spaceBtwnItems / 2),
+            SizedBox(
+              width: 55,
+              child: Text(title, 
+              style: Theme.of(context).textTheme.labelMedium!.apply(color: textColor),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
