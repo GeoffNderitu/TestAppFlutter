16be6e4d4c3c5366dad08b7e0ea32493c3b3ea55
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-23 09:48:17 +0300

    Added content

---
+++ lib/common/widgets/texts/section_heading.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MsSectionHeading extends StatelessWidget {
+  const MsSectionHeading({
+    super.key, 
+    this.textColor, 
+    this.showActionButton = true, 
+    required this.title, 
+    this.buttonTitle = 'View All', 
+    this.onPressed,
+  });
+
+  final Color? textColor;
+  final bool showActionButton;
+  final String title, buttonTitle;
+  final  void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(title, style: Theme.of(context).textTheme.headlineSmall!.apply(color: textColor), maxLines: 1, overflow: TextOverflow.ellipsis),
+        if(showActionButton)TextButton(onPressed: () {}, child: Text(buttonTitle))
+      ],
+    );
+  }
+}
