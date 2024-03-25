2b4e9bdc9bcec43e31cdceb8ca4c0b0eb1ccd1fa
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-25 15:38:47 +0300

    image stingr path changed

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -11,33 +11,33 @@ class MySokoAppImages{
   static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-woman-in-shopping-cart.gif";
   static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-man-delivery.gif";
 
-  static const String sportsIcon = "icons/categories/icons8-soccer-ball-64.png";
-  static const String clothesIcon = "icons/categories/icons8-wardrobe-48.png";
-  static const String shoesIcon = "icons/categories/icons8-sneakers-64.png";
-  static const String cosmeticsIcon = "icons/categories/icons8-cosmetics-96.png";
-  static const String animalsIcon = "icons/categories/icons8-dog-heart-64.png";
-  static const String toysIcon = "icons/categories/icons8-toys-64.png";
-  static const String furnitureIcon = "icons/categories/icons8-furniture-96.png";
-  static const String jeweleryIcon = "icons/categories/icons8-jewelry-100.png";
-  static const String electronicsIcon = "icons/categories/icons8-electronics-48.png";
+  static const String sportsIcon = "assets/icons/categories/icons8-soccer-ball-64.png";
+  static const String clothesIcon = "assets/icons/categories/icons8-wardrobe-48.png";
+  static const String shoesIcon = "assets/icons/categories/icons8-sneakers-64.png";
+  static const String cosmeticsIcon = "assets/icons/categories/icons8-cosmetics-96.png";
+  static const String animalsIcon = "assets/icons/categories/icons8-dog-heart-64.png";
+  static const String toysIcon = "assets/icons/categories/icons8-toys-64.png";
+  static const String furnitureIcon = "assets/icons/categories/icons8-furniture-96.png";
+  static const String jeweleryIcon = "assets/icons/categories/icons8-jewelry-100.png";
+  static const String electronicsIcon = "assets/icons/categories/icons8-electronics-48.png";
 
-  static const String productIllustration = "images/animations/sammy-guy-doing-workout-with-a-tire.png";
-  static const String productSaleIllustration = "images/animations/sammy-34.png";
-  static const String staticSuccessIllustration = "images/animations/bubble-gum-man-successfully-crossing-finish-line.gif";
-  static const String deliveredIllustration = "images/animations/sammy-come-back-later.png";
-  static const String deliveredEmailIllustration = "images/animations/bubble-gum-message-sent.gif";
-  static const String verifyIllustration = "images/animations/sammy-man-is-going-on-a-trip-with-a-suitcase-and-a-passport.png";
+  static const String productIllustration = "assets/images/animations/sammy-guy-doing-workout-with-a-tire.png";
+  static const String productSaleIllustration = "assets/images/animations/sammy-34.png";
+  static const String staticSuccessIllustration = "assets/images/animations/bubble-gum-man-successfully-crossing-finish-line.gif";
+  static const String deliveredIllustration = "assets/images/animations/sammy-come-back-later.png";
+  static const String deliveredEmailIllustration = "assets/images/animations/bubble-gum-message-sent.gif";
+  static const String verifyIllustration = "assets/images/animations/sammy-man-is-going-on-a-trip-with-a-suitcase-and-a-passport.png";
 
-  static const String promoBanner1 = "images/banners/model-promo.jpg";
-  static const String promoBanner2 = "images/banners/nike-shoe-promo.jpg";
-  static const String promoBanner3 = "images/banners/spring-sale.png";
-  static const String banner1 = "images/banners/house-hold-appliances.png";
-  static const String banner2 = "images/banners/nike-shoe-product.jpg";
-  static const String banner3 = "images/banners/tshirt-product-promo.jpg";
-  static const String banner4 = "images/banners/shoes-sale.png";
-  static const String banner5 = "images/banners/electronics-sale.png";
-  static const String banner6 = "images/banners/big-sale.png";
-  static const String banner7 = "images/banners/sammy-delivery-with-gps-navigation.gif";
-  static const String banner8 = "images/banners/dizzy-man-surrounded-by-sale-offers.png";
+  static const String promoBanner1 = "assets/images/banners/model-promo.jpg";
+  static const String promoBanner2 = "assets/images/banners/nike-shoe-promo.jpg";
+  static const String promoBanner3 = "assets/images/banners/spring-sale.png";
+  static const String banner1 = "assets/images/banners/house-hold-appliances.png";
+  static const String banner2 = "assets/images/banners/nike-shoe-product.jpg";
+  static const String banner3 = "assets/images/banners/tshirt-product-promo.jpg";
+  static const String banner4 = "assets/images/banners/shoes-sale.png";
+  static const String banner5 = "assets/images/banners/electronics-sale.png";
+  static const String banner6 = "assets/images/banners/big-sale.png";
+  static const String banner7 = "assets/images/banners/sammy-delivery-with-gps-navigation.gif";
+  static const String banner8 = "assets/images/banners/dizzy-man-surrounded-by-sale-offers.png";
 
 }
