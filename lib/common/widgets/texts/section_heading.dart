93974e52984bc644e4f4159a5a5c1520d7d8ce61
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-27 13:48:34 +0300

    Added space

--- lib/common/widgets/texts/section_heading.dart
+++ lib/common/widgets/texts/section_heading.dart
@@ -18,6 +18,7 @@ class MsSectionHeading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(title, style: Theme.of(context).textTheme.headlineSmall!.apply(color: textColor), maxLines: 1, overflow: TextOverflow.ellipsis),
         if(showActionButton)TextButton(onPressed: () {}, child: Text(buttonTitle))
